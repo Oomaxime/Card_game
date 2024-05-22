5a23e7e66e216b8b8edf09533a2a2632935b0357
Chimisu75 <chems.dabbabi@gmail.com>
2024-05-22 11:25:46 +0200

    new component added

---
+++ src/components/CardEffect.jsx
@@ -0,0 +1,11 @@
+const CardEffect = ({ effect }) => {
+  return (
+    <div className="card-container">
+      <ul className="card-list">
+        <li className="cardItem">{effect}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default CardEffect;
